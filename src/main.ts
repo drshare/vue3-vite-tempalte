b2406c0a839d6c02fcd2a6542818d462ddc0c033
drshare <hjyspace@163.com>
2022-06-28 17:14:15 +0800

    feat(project): add pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
+import store from '@/store';
 import router from '@/router';
 import 'virtual:windi.css';
 import 'element-plus/es/components/message/style/css';
