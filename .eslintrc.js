790c43200ad81ecdeac6ffea5f6b3598b01627cc
drshare <hjyspace@163.com>
2022-06-28 10:20:29 +0800

    feat(project):  Eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+    // 语言环境支持
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    // 扩展
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-base',
+    ],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {
+        indent: [2, 4],
+        'no-undef': [0],
+        'import/no-unresolved': [2, { ignore: ['virtual:generated-pages', 'virtual:windi.css'] }],
+        'import/extensions': [0],
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: true,
+                optionalDependencies: false,
+                peerDependencies: false,
+            },
+        ],
+        'no-shadow': [
+            'error',
+            {
+                builtinGlobals: true,
+                hoist: 'never',
+                allow: [],
+                ignoreOnInitialization: false,
+            },
+        ],
+        'vue/multi-word-component-names': [
+            'error',
+            {
+                ignores: ['login', 'index'],
+            },
+        ],
+    },
+    // 别名设置
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [
+                    ['@', './src'],
+                    ['#', './types'],
+                ],
+                extensions: ['.ts', '.js', '.jsx', '.json'],
+            },
+        },
+    },
+};
--- README.md
+++ README.md
@@ -16,3 +16,33 @@ pnpm create vite vue3-demo -- --template vue-ts
     },
 }
 ```
+
+### 2. 删除多余文件 并且 node 支持 TS
+```shell
+# 安装ts-node
+pnpm i @types/node --save-dev
+```
+
+### 2. 添加代码校验 Eslint
+
+```
+# 安装依赖
+pnpm i eslint eslint-plugin-vue @typescript-eslint/parser eslint-import-resolver-alias --save-dev
+
+# eslint 初始化
+./node_modules/.bin/eslint --init
+```
+√ How would you like to use ESLint? · style
+√ What type of modules does your project use? · esm
+√ Which framework does your project use? · vue
+√ Does your project use TypeScript? · No / Yes
+√ Where does your code run? · browser, node
+√ How would you like to define a style for your project? · guide
+√ Which style guide do you want to follow? · airbnb
+√ What format do you want your config file to be in? · JavaScript
+Checking peerDependencies of eslint-config-airbnb-base@latest
+The config that you've selected requires the following dependencies:
+
+eslint-plugin-vue@latest @typescript-eslint/eslint-plugin@latest eslint-config-airbnb-base@latest eslint@^7.32.0 || ^8.2.0 eslint-plugin-import@^2.25.2 @typescript-eslint/parser@latest
+√ Would you like to install them now? · No / Yes
+√ Which package manager do you want to use? · pnpm
